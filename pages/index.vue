cec71413a645d52a42ac240b0ed514b7ab3c9a19
dkdam <dkdam@live.com>
2022-09-26 23:10:45 +1000

    continuing on the page

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,15 @@
 <template>
-  <div>
-    TODO some apps coming soon
+  <main>
+    <h1>Task board</h1>
+    <p>Create a list of task</p>
+  </main>
+
+  <div class="create-new">
+    <input 
+    type="text" 
+    v-model="newTask" 
+    placeholder="Add a new task" 
+    @keypress.enter="addTask">
   </div>
 </template>
 
