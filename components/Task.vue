8d44d11477e50f751b7378580f113f503571fadc
dkdam <dkdam@live.com>
2022-09-28 23:45:15 +1000

    adding components

---
+++ components/Task.vue
@@ -0,0 +1,19 @@
+<template>
+    <div :class="`task ${task.done ? 'is-complete' : ''}`">
+        <div class="content">{{ task.content }}</div>
+        <div class="buttons">
+            <button>{{ task.done ? 'Undo' : 'Done' }}</button>
+            <button class="delete ">Delete</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -13,7 +13,7 @@
   </div>
 
   <div class="task">
-
+    {{ $store.state.task }}
   </div>
 </template>
 
